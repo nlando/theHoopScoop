d395deb5780a8f01001852e5bea49541d1735431
nlando <nlando@users.noreply.github.com>
2020-02-17 08:03:33 -0800

    Built out more detail and responsive characteristics

---
+++ script/app.js
@@ -0,0 +1,20 @@
+$(() => {
+
+//ideas: - randomize the # of stars that each park has
+// - put name of searched park into review name
+// - give all buttons actions
+// - make add page fill-in options with an array
+
+
+
+
+
+
+
+
+
+
+
+
+
+  });
